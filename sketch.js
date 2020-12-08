1a36ca8a5df98c4cfef757d68096ce3fde1de4ea
nkapoor175 <73940656+nkapoor175@users.noreply.github.com>
2020-12-08 17:05:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var speed,weight;
+var car,wall;
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed = random(55,90);
+  weight = random(400,1500);
+  
+  car = createSprite(50, 200, 50, 50);
+  car.velocityX = speed;
+
+  wall= createSprite(1500, 200, 60, height/2);
+  wall.shapeColor(80,80,80);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
